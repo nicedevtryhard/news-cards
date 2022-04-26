180470b8826a98aa739dbd845af469059dda9c12
nicedevtryhard <artsemkungurtsev@gmail.com>
2022-04-26 15:32:23 +0300

    feat: add header elements

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
-<template>Init</template>
+<template>
+  <app-header></app-header>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import AppHeader from "./components/AppHeader/AppHeader.vue";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { AppHeader },
 });
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Manrope&display=swap");
+* {
+  padding: 0;
+  margin: 0;
+  font-family: "Manrope", sans-serif;
+}
+body {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/AppHeader/AppHeader.vue
@@ -0,0 +1,127 @@
+<template>
+  <header>
+    <nav class="header__nav">
+      <img src="../../assets/logo.png" alt="" />
+      <div class="contacts">
+        <div class="contacts__phone"><span>8 800 000 00 00</span></div>
+        <div class="contacts__mail"><span>sales@logo.ru</span></div>
+      </div>
+    </nav>
+    <flickity ref="flickity" :options="flickityOptions">
+      <div class="carousel-cell">
+        <div class="carousel-cell__ratio"></div>
+        <div class="carousel-cell-content">
+          <h2 class="carousel-cell-content__title">
+            IT-инфраструктура для бизнеса
+          </h2>
+          <h4 class="carousel-cell-content__text">
+            Мы предлагаем комплексные решения для всех уровней бизнеса: от
+            индивидуальных предпринимателей до крупных международных компаний.
+            Более 20 000 клиентов по всему миру доверяют нам.
+          </h4>
+        </div>
+      </div>
+      <div class="carousel-cell">
+        <div class="carousel-cell__ratio"></div>
+        <div class="carousel-cell-content">
+          <h2 class="carousel-cell-content__title">
+            IT-инфраструктура для бизнеса
+          </h2>
+          <h4 class="carousel-cell-content__text">
+            Мы предлагаем комплексные решения для всех уровней бизнеса: от
+            индивидуальных предпринимателей до крупных международных компаний.
+            Более 20 000 клиентов по всему миру доверяют нам.
+          </h4>
+        </div>
+      </div>
+      <div class="carousel-cell">
+        <div class="carousel-cell__ratio"></div>
+        <div class="carousel-cell-content">
+          <h2 class="carousel-cell-content__title">
+            IT-инфраструктура для бизнеса
+          </h2>
+          <h4 class="carousel-cell-content__text">
+            Мы предлагаем комплексные решения для всех уровней бизнеса: от
+            индивидуальных предпринимателей до крупных международных компаний.
+            Более 20 000 клиентов по всему миру доверяют нам.
+          </h4>
+        </div>
+      </div>
+    </flickity>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Flickity from "vue-flickity/src/flickity.vue";
+
+export default defineComponent({
+  components: {
+    Flickity,
+  },
+  setup() {
+    return {};
+  },
+  data() {
+    return {
+      flickityOptions: {
+        initialIndex: 0,
+        contain: true,
+        prevNextButtons: false,
+        pageDots: true,
+        draggable: true,
+        freeScroll: false,
+      },
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.header__nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .contacts {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    &__phone {
+      margin-right: 45px;
+    }
+    &__mail {
+      margin-right: 138px;
+    }
+  }
+}
+.carousel-cell {
+  width: 100%; /* full width */
+  height: 546px;
+  &__ratio {
+    padding-top: 56.25%;
+  }
+  &-content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    padding-left: 255px;
+    background: url(@/assets/Background.png) no-repeat center;
+    background-size: cover;
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+
+    &__title {
+      font-size: 32px;
+    }
+    &__text {
+      font-size: 16px;
+      max-width: 453px;
+    }
+  }
+}
+</style>
